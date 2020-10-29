c5fa1d55df858d6ca7827164e4983ce6a714cfa2
Pedro Arthur <pedro15cg66@gmail.com>
2020-10-29 17:33:07 -0300

    backend uploaded

---
+++ back-end/src/main/java/com/basis/srs/SistemaDeReservaDeSalasApplication.java
@@ -0,0 +1,16 @@
+package com.basis.srs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservaDeSalasApplication {
+
+	public static void main(String[] args) {
+		System.out.println("\uD83D\uDC7D");
+		SpringApplication.run(SistemaDeReservaDeSalasApplication.class, args);
+		System.out.println("\uD83D\uDE80");
+
+	}
+
+}
